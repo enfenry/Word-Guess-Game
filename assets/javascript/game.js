08a2ac19fb6e99e1a89ea72db4cbc15ca9491c34
enfenry <enfenry@vt.edu>
2018-12-15 17:59:12 -0500

    game mechanics complete, needs styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,25 @@
-// Generate random word
-
-let animals = [
-    "Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cat", "Caterpillar", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Clam", "Cobra", "Cockroach", "Cod", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dove", "Dragonfly", "Duck", "Dugong", "Eagle", "Echidna", "Eel", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Goat", "Goldfish", "Goose", "Gorilla", "Grasshopper", "Grouse", "Guanaco", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jellyfish", "Kangaroo", "Kingfisher", "Koala", "Lark", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Louse", "Mallard", "Manatee", "Mandrill", "Mantis", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Rabbit", "Raccoon", "Ram", "Rat", "Raven", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sand Dollar", "Sandpiper", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Shrew", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Squid", "Squirrel", "Starling", "Stingray", "Stinkbug", "Stork", "Swallow", "Swan", "Tapir", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker", "Worm", "Wren", "Yak", "Zebra"
-]
-
-
-
-
-// FOR NOW just using default word
-let wordToGuess = 'hangman'
-
-
-
-
-
-
-let availableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-let guessedLetters = [];
-
 let wins = 0;
-let divWins = document.getElementById("wins");
-divWins.textContent = "Wins: " + wins;
+let losses = 0;
+let availableLetters, guessedLetters, guessesLeft, divUserChoice, divAvailable, divGuessed, divWins, divLosses, divWordShown, wordInBlanks, divGuessesLeft, wordToGuess, gameComplete;
 
+do {
+    guessesLeft = prompt("Select number of attempts (10 = easy, 7 = medium, 5 = hard)");
+} while (isNaN(guessesLeft));
 
-let divAvailable = document.getElementById("available-letters");
-divAvailable.textContent = "Available letters: " + availableLetters;
-
-let divGuessed = document.getElementById("guessed-letters");
-divGuessed.textContent = "Letters already guessed: "
-
-let divWordShown = document.getElementById("guess-word");
-let wordInBlanks = initBlanks(wordToGuess)
-divWordShown.textContent = wordInBlanks;
-
-let divUserChoice = document.getElementById("user-event");
-
-let divGuessesLeft = document.getElementById("guesses-left");
-let guessesLeft = 7;
-divGuessesLeft.textContent = "Incorrect guesses left: " + guessesLeft;
+initRound();
 
 // Get Key input from player
 document.onkeyup = function (event) {
     let chosenLetter = event.key.toUpperCase();
 
-
-    // initialize correctGuess variable as false, will check if it's true.
+    // initialize correctGuess variable as false, will change/check if it's true later.
     let correctGuess = false;
 
+    if (gameComplete) {
+        initRound();
+    }
     // accept Player's Key Input ONLY if input is a letter
-    if (availableLetters.indexOf(chosenLetter) !== -1 && guessesLeft > 0) {
+    else if (availableLetters.indexOf(chosenLetter) !== -1 && guessesLeft > 0) {
 
         //Show letter guessed in divUserChoice
         divUserChoice.textContent = chosenLetter;
@@ -74,6 +45,7 @@ document.onkeyup = function (event) {
             wins += 1;
             divWins.textContent = "Wins: " + wins;
             divUserChoice.textContent = "YOU WIN! TYPE ANY KEY TO PLAY AGAIN.";
+            gameComplete = true;
         }
 
         if (!correctGuess) {
@@ -82,13 +54,58 @@ document.onkeyup = function (event) {
             divGuessesLeft.textContent = "Incorrect guesses left: " + guessesLeft;
 
             if (guessesLeft === 0) {
+                losses += 1;
+                divLosses.textContent = "Losses: " + losses;
                 divUserChoice.textContent = "GAME OVER. TYPE ANY KEY TO PLAY AGAIN.";
+                gameComplete = true;
             }
         }
     }
     else if (guessesLeft != 0) {
         divUserChoice.textContent = "Please guess one of the available letters.";
     }
+}
+
+
+// Start a new round
+function initRound() {
+    // Generate random word
+    let animals = [
+        "Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cat", "Caterpillar", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Clam", "Cobra", "Cockroach", "Cod", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dove", "Dragonfly", "Duck", "Dugong", "Eagle", "Echidna", "Eel", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Goat", "Goldfish", "Goose", "Gorilla", "Grasshopper", "Grouse", "Guanaco", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jellyfish", "Kangaroo", "Kingfisher", "Koala", "Lark", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Louse", "Mallard", "Manatee", "Mandrill", "Mantis", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Rabbit", "Raccoon", "Ram", "Rat", "Raven", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sand Dollar", "Sandpiper", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Shrew", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Squid", "Squirrel", "Starling", "Stingray", "Stinkbug", "Stork", "Swallow", "Swan", "Tapir", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker", "Worm", "Wren", "Yak", "Zebra"
+    ]
+    wordToGuess = animals[Math.floor(Math.random() * animals.length)];
+
+    // FOR NOW just using default word
+    // wordToGuess = 'hangman';
+
+    availableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    guessedLetters = [];
+
+    // initialize gameComplete variable as false, will change/check if it's true later.
+    gameComplete = false;
+
+    divWins = document.getElementById("wins");
+    divWins.textContent = "Wins: " + wins;
+
+    divLosses = document.getElementById("losses");
+    divLosses.textContent = "Losses: " + losses;
+
+
+    divAvailable = document.getElementById("available-letters");
+    divAvailable.textContent = "Available letters: " + availableLetters;
+
+    divGuessed = document.getElementById("guessed-letters");
+    divGuessed.textContent = "Letters already guessed: "
+
+    divWordShown = document.getElementById("guess-word");
+    wordInBlanks = initBlanks(wordToGuess)
+    divWordShown.textContent = wordInBlanks;
+
+    divUserChoice = document.getElementById("user-event");
+    divUserChoice.textContent = "Press any key to get guessin'!";
+
+    divGuessesLeft = document.getElementById("guesses-left");
+    divGuessesLeft.textContent = "Incorrect guesses left: " + guessesLeft;
 }
 
 
