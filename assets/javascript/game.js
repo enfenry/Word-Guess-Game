1f17facac3b61361f9a74e34a9e5f65407e04926
enfenry <enfenry@vt.edu>
2018-12-15 15:21:20 -0500

    letters already guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,13 @@
 let wordToGuess = 'hangman'
 
 let availableLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let guessedLetters = [];
 
 let divAvailable = document.getElementById("available-letters");
-divAvailable.textContent = availableLetters;
+divAvailable.textContent = "Available Letters: " + availableLetters;
+
+let divGuessed = document.getElementById("guessed-letters");
+divGuessed.textContent = "Letters already guessed: "
 
 let divWordShown = document.getElementById("guess-word");
 let wordInBlanks = initBlanks(wordToGuess)
@@ -16,16 +20,18 @@ let divUserChoice = document.getElementById("user-event");
 
 // Get Key input from player
 document.onkeyup = function (event) {
-    let choiceInCaps = event.key.toUpperCase();
+    let chosenLetter = event.key.toUpperCase();
     // accept Player's Key Input ONLY if input is a letter
-    if (availableLetters.indexOf(choiceInCaps) !== -1) {
-        divUserChoice.textContent = choiceInCaps;
-        updateAvailableLetters(choiceInCaps,availableLetters);
-        divAvailable.textContent = availableLetters;
+    if (availableLetters.indexOf(chosenLetter) !== -1) {
+        divUserChoice.textContent = chosenLetter;
+        updateAvailableLetters(chosenLetter,availableLetters);
+        divAvailable.textContent = "Available Letters: " + availableLetters;
+        guessedLetters.push(chosenLetter);
+        divGuessed.textContent = "Letters already guessed: " + guessedLetters;
 
         for (let i = 0; i < wordToGuess.length; i++) {
-            if (choiceInCaps === wordToGuess.charAt(i).toUpperCase()) {
-                wordInBlanks = replaceAt(wordInBlanks,i,choiceInCaps);
+            if (chosenLetter === wordToGuess.charAt(i).toUpperCase()) {
+                wordInBlanks = replaceAt(wordInBlanks,i,chosenLetter);
                 divWordShown.textContent = wordInBlanks;
             }
         }
@@ -41,20 +47,6 @@ function initBlanks(string) {
         blanks = blanks + "_"
     }
     return blanks;
-}
-
-
-// Check if letter is within word
-function containsLetter(letter, word) {
-    let letterInCaps = letter.toUpperCase();
-    let wordInCaps = word.toUpperCase();
-    let containsLetter;
-    if (wordInCaps.indexOf(letterInCaps) === -1) {
-        containsLetter = false;
-    } else {
-        containsLetter = true;
-    }
-    return containsLetter;
 }
 
 // Remove used letter from available letters
